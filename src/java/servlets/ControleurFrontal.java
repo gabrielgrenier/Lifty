3676d20ad0736579e0e65284b63878ace3e1e482
GabrielGrenier <gabriel.braun-grenier@hotmail.com>
2018-10-10 08:37:57 -0400

    Nettoyage du code du controlleur

--- src/java/servlets/ControleurFrontal.java
+++ src/java/servlets/ControleurFrontal.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package servlets;
 
 import controleurs.AbstractAction;
@@ -14,21 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author usager
- */
 public class ControleurFrontal extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
